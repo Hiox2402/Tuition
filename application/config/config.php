231d00142cb6bd06ceb176c3dc5980239d2a9638
Alip <pyro2402@gmail.com>
2023-08-08 14:20:31 +0800

    base url update

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = '';
-$config['base_url'] = "http://localhost/basic-ci2/";
+$config['base_url'] = "http://localhost/WebTuisyen/";
 
 /*
 |--------------------------------------------------------------------------
