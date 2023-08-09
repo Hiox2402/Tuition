786dcf5ae9885b0d6a7ba1dfac7f63a150e3d3ee
Alip <pyro2402@gmail.com>
2023-08-09 14:57:56 +0800

    samples page delete

--- application/views/templates/admin-sidebar.php
+++ application/views/templates/admin-sidebar.php
@@ -83,13 +83,6 @@
 				</ul>
 			</li>
 			<li>
-				<a href="#"><i class="fa fa-files-o fa-fw"></i> Sample Pages<span class="fa arrow"></span></a>
-				<ul class="nav nav-second-level">
-					<li>
-						<a href="#">Blank Page</a>
-					</li>
-					<li>
-						<a href="#">Login Page</a>
 					</li>
 				</ul>
 			</li>
